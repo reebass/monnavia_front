6b5798698f91b57a5f2155e1f5c718c2d07e8347
reebass <store1993dron@gmail.com>
2024-02-28 23:57:46 +0200

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,3 @@
-// import { ButtonMain } from './Buttons/ButtonMain';
-import { Button, Flex, Menu } from 'antd';
-import { DownloadOutlined, HomeOutlined } from '@ant-design/icons';
-import { MainButton, SecondButton } from './Buttons/ButtonMain.styled';
 import { StartPage } from 'pages/StartPage';
 
 export const App = () => {
